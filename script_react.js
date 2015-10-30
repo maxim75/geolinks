47a52c000cbd4922be66284a961a4f0f8a35bca6
Maksym Kozlenko <max@kozlenko.info>
2015-10-30 16:25:21 +1100

    link list

--- script_react.js
+++ script_react.js
@@ -1,17 +1,78 @@
 
 var GeolinksHomePage = React.createClass({
-  render: function() {
-    return (
-      <div className="geolinksHomePage">
-        <h1>Geolinks</h1>
-      </div>
-    );
-  }
+
+
+	render: function() {
+		return (
+			<div className="geolinksHomePage">
+				<h1>Test</h1>
+				<h4>{ this.props.version }</h4>
+				<div>{ 2+2 }</div>
+				<ResourceList />
+			</div>
+		);
+	}
 });
 
+var ResourceList = React.createClass({
+
+
+	render: function() {
+
+		
+
+		var resourceNodes = geolink.resources.map(function (x) {
+			return (
+				<ResourceLink key={x.id} id={x.id}>
+				</ResourceLink>
+			);
+		});
+
+		return (
+			<table className="table">
+				<tbody>
+					{ resourceNodes }
+				</tbody>
+			</table>
+		);
+	}
+});
+
+var ResourceLink = React.createClass({
+
+	getUrl: function() { 
+
+		var data = {
+			lat: parseFloat("1.1"),
+			lng: parseFloat("2.2"),
+			zoom: 18,
+			title: "Title",
+			language: "en"
+		};
+
+		return geolink.getLink(this.props.id, data);
+	},
+
+	render: function() {
+		return (
+			<tr id={this.props.key}>
+				<td >
+					<p>{ this.props.id }</p>
+				</td>
+				<td>
+					<a href={ this.getUrl() }>
+						{ this.getUrl() }
+					</a>
+				</td>
+			</tr>
+		);
+	}
+});
+
+
 ReactDOM.render(
-  <GeolinksHomePage />,
-  document.getElementById('content')
+	<GeolinksHomePage version="0.3" />,
+	document.getElementById('content')
 );
 
 console.log("here");

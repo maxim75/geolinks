799dd54b904a69019d6b44e27077c3578f427447
Maksym Kozlenko <max@kozlenko.info>
2015-12-09 13:21:53 +1100

    uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
 
 gulp.task('browserify', function () {
-	return browserify('./source/app.js')
-
+	return browserify('./source/app.js', { debug: true })
 	.transform(babelify, {presets: ["react"]})
 	.bundle()
 	.pipe(source('geolinkspage.js'))
+	.pipe(buffer())
+	.pipe(sourcemaps.init({ loadMaps: true }))
+	.pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+
 	.pipe(gulp.dest('./build/'));
 
 });
--- source/app.js
+++ source/app.js
@@ -7,16 +7,11 @@ var lodash = require('lodash');
 
 var bootstrap = require('bootstrap');
 
-
-
-
 var GlPage = GlPage || {};
 
-GlPage.isURL = function(str) {
-     var urlRegex = '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
-     var url = new RegExp(urlRegex, 'i');
-     return str.length < 2083 && url.test(str);
-};
+GlPage.ResourceLink = require('./components/ResourceLink.react');
+
+
 
 
 GlPage.setLocationUpdates = function(func) {
@@ -319,38 +314,6 @@ GlPage.ResourceList = React.createClass({
 	}
 });
 
-GlPage.ResourceLink = React.createClass({
-	getUrl: function() { 
-		var link = geolink.getLink(this.props.id, this.props.locationData);
-		return GlPage.isURL(link) ? link : null;
-	},
-
-	getTextValue: function() { 
-		return geolink.getLink(this.props.id, this.props.locationData);
-	},
-
-	render: function() {
-		var anchorOpts = {};
-		var url = this.getUrl();
-
-		if(url) anchorOpts.href = url;
-
-		return (
-			<div className="container">
-				<div className="row link-grid" id={this.props.key}>
-					<div className="col-sm-6 col-xs-12">
-						<strong>{ this.props.id }</strong>
-					</div>
-					<div className="col-sm-6 col-xs-12">
-						<a {...anchorOpts}>
-							{ this.getTextValue() }
-						</a>
-					</div>
-				</div>
-			</div>
-		);
-	}
-});
 
 
 ReactDOM.render(
